3c9a58ffb17066ff707f88c9c61131979fdc1a81
Erik Hofer <erik-hofer@mail.de>
2019-03-09 21:15:34 +0100

    disconnect AudioNodes when an edge is deleted

--- src/store/epic/edge.epic.ts
+++ src/store/epic/edge.epic.ts
@@ -24,6 +24,16 @@ const createEdgeEpic: AppEpic = (action$, _, { graph }) =>
 const deleteEdgeEpic: AppEpic = (action$, _, { graph }) =>
   action$.pipe(
     filter(isActionOf(edgeActions.deleteEdge.request)),
+    tap(action => {
+      const edge = action.payload
+      const source = graph.getNode(edge.sourceNodeId)!
+      const destination = graph.getNode(edge.destinationNodeId)!
+      source.disconnect(
+        destination,
+        edge.sourceOutputIndex,
+        edge.destinationInputIndex
+      )
+    }),
     map(action => graph.removeEdge(action.payload)),
     map(edgeActions.deleteEdge.success)
   )
